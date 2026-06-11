87bf2496a49eddaaf9853b9c03a98cafa8248dea
poomkk234 <27002@bbw.ac.th>
2026-06-11 14:23:02 +0700

    โปรเเกรมรับราคาสินค้า  พร้อมคำนวณตังทอน V.1
    11 มิ.ย. 69
    เข้าใจ py มากยิ่งขึ้น

--- 16tanamongkol.py
+++ 16tanamongkol.py
@@ -1,11 +1,30 @@
-print("hello kub")
-print("my nickname is poom")
-print("i study at bangbowittayacom")
-print("my number is 16")
+price1=float(input("ราคาสินค้าชิ้นที่1: "))
+amount1=int(input("จำนวนสินค้าชิ้นที่1: "))
+price2=float(input("ราคาสินค้าชิ้นที่2: "))
+amount2=int(input("จำนวนสิ้นค้าชิ้นที่2: "))
+total1=(price1*amount1)+ (price2*amount2)
+income=float(input("เงินที่จ่าย: "))
+change=(income-total1)
+print("เงินทอนของคุณ คือ\n" ,change,"บาท")
 
 
 
-name,class_1,number=("poom,4/4,16")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
