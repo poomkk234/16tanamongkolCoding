78612a319ffcd2618a53af4fd0c80bddf6d398ff
poomkk234 <27002@bbw.ac.th>
2026-06-25 13:55:38 +0700

    สอบเก็บคะเเนน
    25/06/2569

---
+++ 16poomkubeiei.py
@@ -0,0 +1,18 @@
+speedcar=int(input("กรอกความเร็วรถของคุณ:  "))
+if speedcar <=80: 
+    print("ปลอดภัย")
+
+elif speedcar <=100: 
+        print("เติอน")
+
+elif speedcar <=120:
+      print("เสี่ยงถูกปรับ")
+
+if speedcar >120: 
+      print("ผิดกฎหมาย(ปรับทันที)")
+
+
+    
+
+
+        
